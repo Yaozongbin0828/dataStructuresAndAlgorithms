ad19bac7ce851e27e4cb16c5efcf869643702fc8
yaozongbin <78633585+yaozongbin@users.noreply.github.com>
2024-03-26 09:46:54 +0800

    Create LeetCode206_翻转链表.java

---
+++ LeetCode/LeetCode206_翻转链表.java
@@ -0,0 +1,44 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList reverser = new ReverseLinkedList();
+
+        // 创建一个链表
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // 翻转链表
+        ListNode reversedList = reverser.reverseList(head);
+
+        // 输出翻转后的链表
+        while (reversedList != null) {
+            System.out.print(reversedList.val + " ");
+            reversedList = reversedList.next;
+        }
+    }
+}
